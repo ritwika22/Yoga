cac2620699b40b548b02b0e74e7f035b44a648a0
Samrat Dutta <samrat00725100@gmail.com>
2022-11-20 11:33:44 +0530

    Hover animation added

--- js/script.js
+++ js/script.js
@@ -9,49 +9,57 @@ function closeNav() {
 }
 
 function lhvr(){
-  document.querySelector('.bi-person-fill-lock').classList.remove("active")
-  document.querySelector('.bi-person-fill-lock').classList.add("hidden")
+  document.querySelector('.bi-person-fill-lock').classList.remove("active");
+  document.querySelector('.bi-person-fill-lock').classList.add("hidden");
 
-  document.querySelector('.hlogin').classList.remove("hidden")
-  document.querySelector('.hlogin').classList.add("active")
+  document.querySelector('.hlogin').classList.remove("hidden");
+  document.querySelector('.hlogin').classList.add("active");
 }
 
 function lhvro(){
-  document.querySelector('.bi-person-fill-lock').classList.add("active")
-  document.querySelector('.bi-person-fill-lock').classList.remove("hidden")
+  document.querySelector('.bi-person-fill-lock').classList.add("active");
+  document.querySelector('.bi-person-fill-lock').classList.remove("hidden");
 
-  document.querySelector('.hlogin').classList.add("hidden")
-  document.querySelector('.hlogin').classList.remove("active")
+  document.querySelector('.hlogin').classList.add("hidden");
+  document.querySelector('.hlogin').classList.remove("active");
 }
 
 function chvr(){
-  document.querySelector('.bi-cart-plus-fill').classList.remove("active")
-  document.querySelector('.bi-cart-plus-fill').classList.add("hidden")
+  document.querySelector('.bi-cart-plus-fill').classList.remove("active");
+  document.querySelector('.bi-cart-plus-fill').classList.add("hidden");
 
-  document.querySelector('.hcart').classList.remove("hidden")
-  document.querySelector('.hcart').classList.add("active")
+  document.querySelector('.hcart').classList.remove("hidden");
+  document.querySelector('.hcart').classList.add("active");
 }
 
 function chvro(){
-  document.querySelector('.bi-cart-plus-fill').classList.add("active")
-  document.querySelector('.bi-cart-plus-fill').classList.remove("hidden")
+  document.querySelector('.bi-cart-plus-fill').classList.add("active");
+  document.querySelector('.bi-cart-plus-fill').classList.remove("hidden");
 
-  document.querySelector('.hcart').classList.add("hidden")
-  document.querySelector('.hcart').classList.remove("active")
+  document.querySelector('.hcart').classList.add("hidden");
+  document.querySelector('.hcart').classList.remove("active");
 }
 
 function shvr(){
-  document.querySelector('.bi-search').classList.remove("active")
-  document.querySelector('.bi-search').classList.add("hidden")
+  document.querySelector('.bi-search').classList.remove("active");
+  document.querySelector('.bi-search').classList.add("hidden");
 
-  document.querySelector('.hsearch').classList.remove("hidden")
-  document.querySelector('.hsearch').classList.add("active")
+  document.querySelector('.hsearch').classList.remove("hidden");
+  document.querySelector('.hsearch').classList.add("active");
 }
 
 function shvro(){
-  document.querySelector('.bi-search').classList.add("active")
+  document.querySelector('.bi-search').classList.add("active");
   document.querySelector('.bi-search').classList.remove("hidden")
 
-  document.querySelector('.hsearch').classList.add("hidden")
-  document.querySelector('.hsearch').classList.remove("active")
+  document.querySelector('.hsearch').classList.add("hidden");
+  document.querySelector('.hsearch').classList.remove("active");
+}
+
+function rotateimg(e){
+  e.classList.add("bg-rotate");
+}
+
+function stopimg(e){
+  e.classList.remove("bg-rotate");
 }
